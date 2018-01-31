9f2357644d21aaf207165ea6ad714d57154951e5
drd387 <dannyd40@gmail.com>
2018-01-30 21:00:05 -0500

    made classes extend Model

--- src/main/java/org/dselent/scheduling/server/model/CourseSchedule.java
+++ src/main/java/org/dselent/scheduling/server/model/CourseSchedule.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CourseSchedule {
+public class CourseSchedule extends Model{
 	//table name
 	public static final String TABLE_NAME = "instructors";
 
--- src/main/java/org/dselent/scheduling/server/model/CourseSection.java
+++ src/main/java/org/dselent/scheduling/server/model/CourseSection.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class CourseSection {
+public class CourseSection extends Model{
 	
 	//table name
 	public static final String TABLE_NAME = "instructors";
