c4e3c2584cbac60f5747d0a5a74a91d938156cc5
Javier Marcos <xbc30ep450@gmail.com>
2018-01-31 14:31:05 -0500

    Created AdminInbox Extractor

---
+++ src/main/java/org/dselent/scheduling/server/extractor/AdminInboxExtractor.java
@@ -0,0 +1,50 @@
+package org.dselent.scheduling.server.extractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.dselent.scheduling.server.model.AdminInbox;
+
+public class AdminInboxExtractor extends Extractor<List<AdminInbox>>
+{
+	@Override
+	public List<AdminInbox> extractData(ResultSet rs) throws SQLException
+	{
+		List<AdminInbox> resultList = new ArrayList<>();
+
+		while(rs.next())
+		{
+			AdminInbox result = new AdminInbox();
+			
+			result.setId(rs.getInt(AdminInbox.getColumnName(AdminInbox.Columns.ID)));
+			result.setId(rs.getInt(AdminInbox.getColumnName(AdminInbox.Columns.INBOX_USER)));
+			result.setId(rs.getInt(AdminInbox.getColumnName(AdminInbox.Columns.SENDER)));
+			
+			if(rs.wasNull())
+			{
+				result.setId(null);
+				result.setInboxUser(null);
+				result.setSender(null);
+			}
+			
+			result.setSubjectLine(rs.getString(AdminInbox.getColumnName(AdminInbox.Columns.SUBJECT_LINE)));
+			result.setContent(rs.getString(AdminInbox.getColumnName(AdminInbox.Columns.CONTENT)));
+			result.setStatus(rs.getBoolean(AdminInbox.getColumnName(AdminInbox.Columns.STATUS)));
+			
+			if(rs.wasNull())
+			{
+				result.setStatus(null);
+			}
+			
+			result.setCreatedAt(rs.getTimestamp(AdminInbox.getColumnName(AdminInbox.Columns.CREATED_AT)));
+			result.setUpdatedAt(rs.getTimestamp(AdminInbox.getColumnName(AdminInbox.Columns.UPDATED_AT)));
+		
+			resultList.add(result);
+		}
+			
+		return resultList;
+	}
+
+}
