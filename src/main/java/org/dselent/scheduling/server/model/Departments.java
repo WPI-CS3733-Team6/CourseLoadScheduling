7fdb483fc709f25e617d92aab4b0f16e268dce44
bobbyshome <bobbyshome@autoreg-070211.dyn.wpi.edu>
2018-01-30 21:17:52 -0500

    adding departments model

---
+++ src/main/java/org/dselent/scheduling/server/model/Departments.java
@@ -0,0 +1,124 @@
+package org.dselent.scheduling.server.model;
+
+import java.sql.JDBCType;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dselent.scheduling.server.model.CourseInformation.Columns;
+
+
+public class Departments extends Model {
+	
+	//table name
+	public static final String TABLE_NAME = "departments";
+	
+	//column names
+	public static enum Columns {
+		ID,
+		DEPT_NAME,
+		CREATED_AT,
+		UPDATED_AT
+	}
+	
+	//enum list
+	private static final List<Columns> COLUMN_LIST = new ArrayList<>();
+	
+	// type mapping
+	private static final Map<Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+	
+	static {
+		for(Columns key: Columns.values()) {
+			COLUMN_LIST.add(key);
+		}
+		
+		COLUMN_TYPE_MAP.put(Columns.ID, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.DEPT_NAME, JDBCType.VARCHAR);
+		COLUMN_TYPE_MAP.put(Columns.CREATED_AT, JDBCType.TIMESTAMP);
+		COLUMN_TYPE_MAP.put(Columns.UPDATED_AT, JDBCType.TIMESTAMP);
+	}
+	
+	//attributes
+	
+	private Integer id;
+	private String dept_name;
+	private Timestamp created_at;
+	private Timestamp updated_at;
+	
+	//methods
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDept_name() {
+		return dept_name;
+	}
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+	public Timestamp getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(Timestamp created_at) {
+		this.created_at = created_at;
+	}
+	public Timestamp getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(Timestamp updated_at) {
+		this.updated_at = updated_at;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((created_at == null) ? 0 : created_at.hashCode());
+		result = prime * result + ((dept_name == null) ? 0 : dept_name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((updated_at == null) ? 0 : updated_at.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Departments other = (Departments) obj;
+		if (created_at == null) {
+			if (other.created_at != null)
+				return false;
+		} else if (!created_at.equals(other.created_at))
+			return false;
+		if (dept_name == null) {
+			if (other.dept_name != null)
+				return false;
+		} else if (!dept_name.equals(other.dept_name))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (updated_at == null) {
+			if (other.updated_at != null)
+				return false;
+		} else if (!updated_at.equals(other.updated_at))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Departments [id=" + id + ", dept_name=" + dept_name + ", created_at=" + created_at + ", updated_at="
+				+ updated_at + "]";
+	}
+	
+}
