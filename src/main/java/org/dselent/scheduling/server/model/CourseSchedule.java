baa1f62d54fe065fd848bc7500f87f107c9e343f
drd387 <dannyd40@gmail.com>
2018-01-30 20:35:42 -0500

    added CourseSchedule

---
+++ src/main/java/org/dselent/scheduling/server/model/CourseSchedule.java
@@ -0,0 +1,109 @@
+package org.dselent.scheduling.server.model;
+
+import java.sql.JDBCType;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CourseSchedule {
+	//table name
+	public static final String TABLE_NAME = "instructors";
+
+	//column names
+	public static enum Columns
+	{
+		ID,
+		SECTION_ID,
+		TYPE,
+		MEETING_DAYS,
+		TIME_START,
+		TIME_END,
+		CREATED_AT,
+		UPDATED_AT
+	}
+
+	// enum list
+	private static final List<Columns> COLUMN_LIST = new ArrayList<>();
+
+	// type mapping
+	private static final Map<Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+
+	static
+	{
+		for(Columns key : Columns.values())
+		{
+			COLUMN_LIST.add(key);
+		}
+
+		COLUMN_TYPE_MAP.put(Columns.ID, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.SECTION_ID, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.TYPE, JDBCType.VARCHAR);
+		COLUMN_TYPE_MAP.put(Columns.MEETING_DAYS, JDBCType.VARCHAR);
+		COLUMN_TYPE_MAP.put(Columns.TIME_START, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.TIME_END, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.CREATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+		COLUMN_TYPE_MAP.put(Columns.UPDATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+	};
+
+	// attributes
+	private String type;
+	private String meetingDays;
+	private Integer timeStart;
+	private Integer timeEnd;
+	private Instant createdAt;
+	private Instant updatedAt;
+	private Integer id;
+	private Integer sectionId;
+
+
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getSectionId() {
+		return sectionId;
+	}
+	public void setSectionId(Integer sectionId) {
+		this.sectionId = sectionId;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getMeetingDays() {
+		return meetingDays;
+	}
+	public void setMeetingDays(String meetingDays) {
+		this.meetingDays = meetingDays;
+	}
+	public Integer getTimeStart() {
+		return timeStart;
+	}
+	public void setTimeStart(Integer timeStart) {
+		this.timeStart = timeStart;
+	}
+	public Integer getTimeEnd() {
+		return timeEnd;
+	}
+	public void setTimeEnd(Integer timeEnd) {
+		this.timeEnd = timeEnd;
+	}
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
