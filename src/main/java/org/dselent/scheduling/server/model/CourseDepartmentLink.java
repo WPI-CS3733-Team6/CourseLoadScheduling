5800ea41d8a81be5fa0015320488c650b006828d
drd387 <dannyd40@gmail.com>
2018-01-31 14:15:21 -0500

    added courseDepartmentLink

---
+++ src/main/java/org/dselent/scheduling/server/model/CourseDepartmentLink.java
@@ -0,0 +1,135 @@
+package org.dselent.scheduling.server.model;
+
+import java.sql.JDBCType;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.dselent.scheduling.server.model.CourseInformation.Columns;
+
+public class CourseDepartmentLink {
+	//table name
+	public static final String TABLE_NAME = "course_department_link";
+
+	//column names
+	public static enum Columns {
+		ID,
+		COURSE_ID,
+		DEPT_ID,
+		CREATED_AT,
+		UPDATED_AT
+	}
+
+	//enum list
+	private static final List<Columns> COLUMN_LIST = new ArrayList<>();
+
+	// type mapping
+	private static final Map<Columns, JDBCType> COLUMN_TYPE_MAP = new HashMap<>();
+
+	static
+	{
+		for(Columns key: Columns.values()) {
+			COLUMN_LIST.add(key);
+		}
+
+		COLUMN_TYPE_MAP.put(Columns.ID, JDBCType.INTEGER);
+		COLUMN_TYPE_MAP.put(Columns.COURSE_ID, JDBCType.VARCHAR);
+		COLUMN_TYPE_MAP.put(Columns.DEPT_ID, JDBCType.VARCHAR);
+		COLUMN_TYPE_MAP.put(Columns.CREATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+		COLUMN_TYPE_MAP.put(Columns.UPDATED_AT, JDBCType.TIMESTAMP_WITH_TIMEZONE);
+	};
+
+	//attributes
+
+	private Integer id;
+	private String courseId;
+	private String deptId;
+	private Instant createdAt;
+	private Instant updatedAt;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public String getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(String deptId) {
+		this.deptId = deptId;
+	}
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Instant createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Instant updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((courseId == null) ? 0 : courseId.hashCode());
+		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+		result = prime * result + ((deptId == null) ? 0 : deptId.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CourseDepartmentLink other = (CourseDepartmentLink) obj;
+		if (courseId == null) {
+			if (other.courseId != null)
+				return false;
+		} else if (!courseId.equals(other.courseId))
+			return false;
+		if (createdAt == null) {
+			if (other.createdAt != null)
+				return false;
+		} else if (!createdAt.equals(other.createdAt))
+			return false;
+		if (deptId == null) {
+			if (other.deptId != null)
+				return false;
+		} else if (!deptId.equals(other.deptId))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (updatedAt == null) {
+			if (other.updatedAt != null)
+				return false;
+		} else if (!updatedAt.equals(other.updatedAt))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "CourseDepartmentLink [id=" + id + ", courseId=" + courseId + ", deptId=" + deptId + ", createdAt="
+				+ createdAt + ", updatedAt=" + updatedAt + "]";
+	}
+	
+	
+}
