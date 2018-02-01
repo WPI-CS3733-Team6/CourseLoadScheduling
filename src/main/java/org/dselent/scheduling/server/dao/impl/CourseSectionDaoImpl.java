bf87c8219e1a221e15383dac9d6b574ab1b3957c
drd387 <dannyd40@gmail.com>
2018-01-31 23:36:35 -0500

    added CourseSectionDaoImpl

---
+++ src/main/java/org/dselent/scheduling/server/dao/impl/CourseSectionDaoImpl.java
@@ -0,0 +1,245 @@
+package org.dselent.scheduling.server.dao.impl;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.dselent.scheduling.server.dao.CourseSectionDao;
+import org.dselent.scheduling.server.extractor.CourseSectionExtractor;
+import org.dselent.scheduling.server.miscellaneous.Pair;
+import org.dselent.scheduling.server.miscellaneous.QueryStringBuilder;
+import org.dselent.scheduling.server.model.CourseSection;
+import org.dselent.scheduling.server.sqlutils.ColumnOrder;
+import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
+import org.dselent.scheduling.server.sqlutils.QueryTerm;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+public class CourseSectionDaoImpl  extends BaseDaoImpl<CourseSection> implements CourseSectionDao{
+	@Override
+	public int insert(CourseSection CourseSectionModel, List<String> insertColumnNameList, List<String> keyHolderColumnNameList) throws SQLException
+	{
+		
+		validateColumnNames(insertColumnNameList);
+		validateColumnNames(keyHolderColumnNameList);
+
+		String queryTemplate = QueryStringBuilder.generateInsertString(CourseSection.TABLE_NAME, insertColumnNameList);
+	    MapSqlParameterSource parameters = new MapSqlParameterSource();
+	    
+	    List<Map<String, Object>> keyList = new ArrayList<>();
+	    KeyHolder keyHolder = new GeneratedKeyHolder(keyList);
+	    
+	    for(String insertColumnName : insertColumnNameList)
+	    {
+	    	addParameterMapValue(parameters, insertColumnName, CourseSectionModel);
+	    }
+	    // new way, but unfortunately unnecessary class creation is slow and wasteful (i.e. wrong)
+	    // insertColumnNames.forEach(insertColumnName -> addParameterMap(parameters, insertColumnName, userModel));
+	    
+	    int rowsAffected = namedParameterJdbcTemplate.update(queryTemplate, parameters, keyHolder);
+	    
+	    Map<String, Object> keyMap = keyHolder.getKeys();
+	    
+	    for(String keyHolderColumnName : keyHolderColumnNameList)
+	    {
+	    	addObjectValue(keyMap, keyHolderColumnName, CourseSectionModel);
+	    }
+	    	    
+	    return rowsAffected;
+		
+	}
+	
+	
+	@Override
+	public List<CourseSection> select(List<String> selectColumnNameList, List<QueryTerm> queryTermList, List<Pair<String, ColumnOrder>> orderByList) throws SQLException
+	{
+		CourseSectionExtractor extractor = new CourseSectionExtractor();
+		String queryTemplate = QueryStringBuilder.generateSelectString(CourseSection.TABLE_NAME, selectColumnNameList, queryTermList, orderByList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    List<CourseSection> CourseSectionList = jdbcTemplate.query(queryTemplate, extractor, parameters);
+	    
+	    return CourseSectionList;
+	}
+
+	@Override
+	public CourseSection findById(int id) throws SQLException
+	{
+		String columnName = QueryStringBuilder.convertColumnName(CourseSection.getColumnName(CourseSection.Columns.ID), false);
+		List<String> selectColumnNames = CourseSection.getColumnNameList();
+		
+		List<QueryTerm> queryTermList = new ArrayList<>();
+		QueryTerm idTerm = new QueryTerm(columnName, ComparisonOperator.EQUAL, id, null);
+		queryTermList.add(idTerm);
+		
+		List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
+		Pair<String, ColumnOrder> order = new Pair<String, ColumnOrder>(columnName, ColumnOrder.ASC);
+		orderByList.add(order);
+		
+		List<CourseSection> CourseSectionList = select(selectColumnNames, queryTermList, orderByList);
+	
+		CourseSection CourseSection = null;
+	    
+	    if(!CourseSectionList.isEmpty())
+	    {
+	    	CourseSection = CourseSectionList.get(0);
+	    }
+	    
+	    return CourseSection;
+	}
+	
+	@Override
+	public int update(String columnName, Object newValue, List<QueryTerm> queryTermList)
+	{
+		String queryTemplate = QueryStringBuilder.generateUpdateString(CourseSection.TABLE_NAME, columnName, queryTermList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		objectList.add(newValue);
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+	    
+		return rowsAffected;
+	}
+	
+	@Override
+	public int delete(List<QueryTerm> queryTermList)
+	{
+		String queryTemplate = QueryStringBuilder.generateDeleteString(CourseSection.TABLE_NAME, queryTermList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+	    
+		return rowsAffected;
+	}
+
+	private void addParameterMapValue(MapSqlParameterSource parameters, String insertColumnName, CourseSection CourseSectionModel)
+	{
+		String parameterName = QueryStringBuilder.convertColumnName(insertColumnName, false);
+    	
+    	// Wish this could generalize
+    	// The getter must be distinguished unless the models are designed as simply a map of columns-values
+    	// Would prefer not being that generic since it may end up leading to all code being collections of strings
+		
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.ID)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getId());
+    	}
+    	
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.COURSE_NUM)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getCourseNum());
+    	}
+    	
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.SECTION_NUM)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getSectionNum());
+    	}
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.TERM)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getTerm());
+    	}
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.EXPECTED_POP)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getExpectedPop());
+    	}
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.DELETED)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getDeleted());
+    	}
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.CREATED_AT)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getCreatedAt());
+    	}
+    	if(insertColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.UPDATED_AT)))
+    	{
+    		parameters.addValue(parameterName, CourseSectionModel.getUpdatedAt());
+    	}
+    	
+    	else
+    	{
+    		// should never end up here
+    		// lists should have already been validated
+    		throw new IllegalArgumentException("Invalid column name provided: " + insertColumnName);
+    	}
+	}	
+
+	private void addObjectValue(Map<String, Object> keyMap, String keyHolderColumnName, CourseSection CourseSectionModel)
+	{
+    	if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.ID)))
+    	{
+    		CourseSectionModel.setId((Integer) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.COURSE_NUM)))
+    	{
+    		CourseSectionModel.setCourseNum((String) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.SECTION_NUM)))
+    	{
+    		CourseSectionModel.setSectionNum((Integer) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.TERM)))
+    	{
+    		CourseSectionModel.setTerm((String) keyMap.get(keyHolderColumnName));
+    	}else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.EXPECTED_POP)))
+    	{
+    		CourseSectionModel.setExpectedPop((Integer) keyMap.get(keyHolderColumnName));
+    	}else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.DELETED)))
+    	{
+    		CourseSectionModel.setDeleted((Boolean) keyMap.get(keyHolderColumnName));
+    	}else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.CREATED_AT)))
+    	{
+    		CourseSectionModel.setCreatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+    	}else if(keyHolderColumnName.equals(CourseSection.getColumnName(CourseSection.Columns.UPDATED_AT)))
+    	{
+    		CourseSectionModel.setUpdatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+    	}
+    	else
+    	{
+    		// should never end up here
+    		// lists should have already been validated
+    		throw new IllegalArgumentException("Invalid column name provided: " + keyHolderColumnName);
+    	}
+	}
+	
+	@Override
+	public void validateColumnNames(List<String> columnNameList)
+	{
+		List<String> actualColumnNames = CourseSection.getColumnNameList();
+		boolean valid = actualColumnNames.containsAll(columnNameList);
+		
+		if(!valid)
+		{
+			List<String> invalidColumnNames = new ArrayList<>(columnNameList);
+			invalidColumnNames.removeAll(actualColumnNames);
+			
+			throw new IllegalArgumentException("Invalid column names provided: " + invalidColumnNames);
+		}
+	}
+}
