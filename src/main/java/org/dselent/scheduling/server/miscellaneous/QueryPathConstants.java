c8974f1ab3a9b2c9f486da33b7dd73dac0d7a9ce
Josue Contreras <josudaco@hotmail.com>
2018-02-01 17:47:07 -0500

    Accesses to advanced querys-Josue

--- src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
+++ src/main/java/org/dselent/scheduling/server/miscellaneous/QueryPathConstants.java
@@ -19,8 +19,26 @@ public class QueryPathConstants
 	private static String BASE_QUERY_PATH = "sql" + File.separator + "Active" + File.separator;
 	private static String SQL_EXTENSION = ".sql";
 
+	//CustomUsersWithRole (example from professor)
 	private static String USERS_WITH_ROLE_PATH = BASE_QUERY_PATH + "CustomUsersWithRole" + SQL_EXTENSION;
-
+	
+	//new advanced query code:
+	//AccountInformation
+	private static String ACCOUNT_INFORMATION_PATH = BASE_QUERY_PATH + "AccountInformation" + SQL_EXTENSION;
+	//AdvancedSearchDetailed
+	private static String AVANCED_SEARCH_DETAILED_PATH = BASE_QUERY_PATH + "AdvancedSearchDetailed" + SQL_EXTENSION;
+	//CourseScheduleInfromation
+	private static String COURSE_SCHEDULE_INFORMATION_PATH = BASE_QUERY_PATH + "CourseScheduleInformation" + SQL_EXTENSION;
+	//HomepageCart
+	private static String HOMEPAGE_CART_PATH = BASE_QUERY_PATH + "HomepageCart" + SQL_EXTENSION;
+	//RegistrationCart
+	private static String REGISTRATION_CART_PATH = BASE_QUERY_PATH + "RegistrationCart" + SQL_EXTENSION;
+	//SearchClasses
+	private static String SERACH_CLASSES_PATH = BASE_QUERY_PATH + "SearchClasses" + SQL_EXTENSION;
+	//UserHomepageRegistered
+	private static String USER_HOMEPAGE_REGISTERED_PATH = BASE_QUERY_PATH + "UserHomepageRegistered" + SQL_EXTENSION;
+	//ViewClasses
+	private static String VIEW_CLASSES_PATH = BASE_QUERY_PATH + "ViewClasses" + SQL_EXTENSION;
 	
 	/////////////////////////////////////////////////////////////////////////////////////////////////
 	
