6c27274519a1c177d967aa3fcd1031bbcd057d67
drd387 <dannyd40@gmail.com>
2018-01-31 22:55:10 -0500

    added departments daoimpl

---
+++ src/main/java/org/dselent/scheduling/server/dao/impl/DepartmentsDaoImpl.java
@@ -0,0 +1,214 @@
+package org.dselent.scheduling.server.dao.impl;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.dselent.scheduling.server.dao.DepartmentsDao;
+import org.dselent.scheduling.server.extractor.DepartmentsExtractor;
+import org.dselent.scheduling.server.miscellaneous.Pair;
+import org.dselent.scheduling.server.miscellaneous.QueryStringBuilder;
+import org.dselent.scheduling.server.model.Departments;
+import org.dselent.scheduling.server.sqlutils.ColumnOrder;
+import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
+import org.dselent.scheduling.server.sqlutils.QueryTerm;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+public class DepartmentsDaoImpl extends BaseDaoImpl<Departments> implements DepartmentsDao
+{
+	@Override
+	public int insert(Departments DepartmentsModel, List<String> insertColumnNameList, List<String> keyHolderColumnNameList) throws SQLException
+	{
+		
+		validateColumnNames(insertColumnNameList);
+		validateColumnNames(keyHolderColumnNameList);
+
+		String queryTemplate = QueryStringBuilder.generateInsertString(Departments.TABLE_NAME, insertColumnNameList);
+	    MapSqlParameterSource parameters = new MapSqlParameterSource();
+	    
+	    List<Map<String, Object>> keyList = new ArrayList<>();
+	    KeyHolder keyHolder = new GeneratedKeyHolder(keyList);
+	    
+	    for(String insertColumnName : insertColumnNameList)
+	    {
+	    	addParameterMapValue(parameters, insertColumnName, DepartmentsModel);
+	    }
+	    // new way, but unfortunately unnecessary class creation is slow and wasteful (i.e. wrong)
+	    // insertColumnNames.forEach(insertColumnName -> addParameterMap(parameters, insertColumnName, userModel));
+	    
+	    int rowsAffected = namedParameterJdbcTemplate.update(queryTemplate, parameters, keyHolder);
+	    
+	    Map<String, Object> keyMap = keyHolder.getKeys();
+	    
+	    for(String keyHolderColumnName : keyHolderColumnNameList)
+	    {
+	    	addObjectValue(keyMap, keyHolderColumnName, DepartmentsModel);
+	    }
+	    	    
+	    return rowsAffected;
+		
+	}
+	
+	
+	@Override
+	public List<Departments> select(List<String> selectColumnNameList, List<QueryTerm> queryTermList, List<Pair<String, ColumnOrder>> orderByList) throws SQLException
+	{
+		DepartmentsExtractor extractor = new DepartmentsExtractor();
+		String queryTemplate = QueryStringBuilder.generateSelectString(Departments.TABLE_NAME, selectColumnNameList, queryTermList, orderByList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    List<Departments> DepartmentsList = jdbcTemplate.query(queryTemplate, extractor, parameters);
+	    
+	    return DepartmentsList;
+	}
+
+	@Override
+	public Departments findById(int id) throws SQLException
+	{
+		String columnName = QueryStringBuilder.convertColumnName(Departments.getColumnName(Departments.Columns.ID), false);
+		List<String> selectColumnNames = Departments.getColumnNameList();
+		
+		List<QueryTerm> queryTermList = new ArrayList<>();
+		QueryTerm idTerm = new QueryTerm(columnName, ComparisonOperator.EQUAL, id, null);
+		queryTermList.add(idTerm);
+		
+		List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
+		Pair<String, ColumnOrder> order = new Pair<String, ColumnOrder>(columnName, ColumnOrder.ASC);
+		orderByList.add(order);
+		
+		List<Departments> DepartmentsList = select(selectColumnNames, queryTermList, orderByList);
+	
+		Departments Departments = null;
+	    
+	    if(!DepartmentsList.isEmpty())
+	    {
+	    	Departments = DepartmentsList.get(0);
+	    }
+	    
+	    return Departments;
+	}
+	
+	@Override
+	public int update(String columnName, Object newValue, List<QueryTerm> queryTermList)
+	{
+		String queryTemplate = QueryStringBuilder.generateUpdateString(Departments.TABLE_NAME, columnName, queryTermList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		objectList.add(newValue);
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+	    
+		return rowsAffected;
+	}
+	
+	@Override
+	public int delete(List<QueryTerm> queryTermList)
+	{
+		String queryTemplate = QueryStringBuilder.generateDeleteString(Departments.TABLE_NAME, queryTermList);
+
+		List<Object> objectList = new ArrayList<Object>();
+		
+		for(QueryTerm queryTerm : queryTermList)
+		{
+			objectList.add(queryTerm.getValue());
+		}
+		
+	    Object[] parameters = objectList.toArray();
+		 
+	    int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
+	    
+		return rowsAffected;
+	}
+
+	private void addParameterMapValue(MapSqlParameterSource parameters, String insertColumnName, Departments DepartmentsModel)
+	{
+		String parameterName = QueryStringBuilder.convertColumnName(insertColumnName, false);
+    	
+    	// Wish this could generalize
+    	// The getter must be distinguished unless the models are designed as simply a map of columns-values
+    	// Would prefer not being that generic since it may end up leading to all code being collections of strings
+		
+    	if(insertColumnName.equals(Departments.getColumnName(Departments.Columns.ID)))
+    	{
+    		parameters.addValue(parameterName, DepartmentsModel.getId());
+    	}
+    	else if(insertColumnName.equals(Departments.getColumnName(Departments.Columns.DEPT_NAME)))
+    	{
+    		parameters.addValue(parameterName, DepartmentsModel.getDeptName());
+    	}
+    	else if(insertColumnName.equals(Departments.getColumnName(Departments.Columns.CREATED_AT)))
+    	{
+    		parameters.addValue(parameterName, DepartmentsModel.getCreatedAt());
+    	}
+    	else if(insertColumnName.equals(Departments.getColumnName(Departments.Columns.UPDATED_AT)))
+    	{
+    		parameters.addValue(parameterName, DepartmentsModel.getUpdatedAt());
+    	}
+    	else
+    	{
+    		// should never end up here
+    		// lists should have already been validated
+    		throw new IllegalArgumentException("Invalid column name provided: " + insertColumnName);
+    	}
+	}	
+
+	private void addObjectValue(Map<String, Object> keyMap, String keyHolderColumnName, Departments DepartmentsModel)
+	{
+    	if(keyHolderColumnName.equals(Departments.getColumnName(Departments.Columns.ID)))
+    	{
+    		DepartmentsModel.setId((Integer) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(Departments.getColumnName(Departments.Columns.DEPT_NAME)))
+    	{
+    		DepartmentsModel.setDeptName((String) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(Departments.getColumnName(Departments.Columns.CREATED_AT)))
+    	{
+    		DepartmentsModel.setCreatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+    	}
+    	else if(keyHolderColumnName.equals(Departments.getColumnName(Departments.Columns.UPDATED_AT)))
+    	{
+    		DepartmentsModel.setUpdatedAt((Timestamp) keyMap.get(keyHolderColumnName));
+    	}
+    	else
+    	{
+    		// should never end up here
+    		// lists should have already been validated
+    		throw new IllegalArgumentException("Invalid column name provided: " + keyHolderColumnName);
+    	}
+	}
+	
+	@Override
+	public void validateColumnNames(List<String> columnNameList)
+	{
+		List<String> actualColumnNames = Departments.getColumnNameList();
+		boolean valid = actualColumnNames.containsAll(columnNameList);
+		
+		if(!valid)
+		{
+			List<String> invalidColumnNames = new ArrayList<>(columnNameList);
+			invalidColumnNames.removeAll(actualColumnNames);
+			
+			throw new IllegalArgumentException("Invalid column names provided: " + invalidColumnNames);
+		}
+	}
+}
