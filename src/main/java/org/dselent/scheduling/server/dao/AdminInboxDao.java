5c9a459845f59babf500cc892b7fbb4eb03b7714
Javier Marcos <xbc30ep450@gmail.com>
2018-02-02 20:05:49 -0500

    Added update2.0 to AdminInboxDao

--- src/main/java/org/dselent/scheduling/server/dao/AdminInboxDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/AdminInboxDao.java
@@ -1,10 +1,10 @@
 package org.dselent.scheduling.server.dao;
+import java.util.List;
+
 import org.dselent.scheduling.server.model.AdminInbox;
+import org.dselent.scheduling.server.sqlutils.QueryTerm;
 
 public interface AdminInboxDao extends Dao<AdminInbox>{
 
-	//Update the status field of the specified mail
-	//True for read, False for unread
-	int markAsRead(int id, boolean newStatus);
-	//Add functions as needed
+	public int updateMail(List<String> columnNameList, List<Object> newValueList, List<QueryTerm> queryTermList);
 }
--- src/main/java/org/dselent/scheduling/server/dao/impl/AdminInboxDaoImpl.java
+++ src/main/java/org/dselent/scheduling/server/dao/impl/AdminInboxDaoImpl.java
@@ -2,6 +2,7 @@ package org.dselent.scheduling.server.dao.impl;
 
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -63,27 +64,25 @@ public class AdminInboxDaoImpl extends BaseDaoImpl<AdminInbox> implements AdminI
 		
 	}
 	
-	@Override
-	public int markAsRead(int id, boolean newStatus) {
-	    
-		List<QueryTerm> queryTermList = new ArrayList<QueryTerm>();
+	public int updateMail(List<String> columnNameList, List<Object> newValueList, List<QueryTerm> queryTermList) {
+		List<Integer> typeList = new ArrayList<Integer>();
+		typeList.add(Types.VARCHAR);
+		
+		String queryTemplate = QueryStringBuilder.generateUpdateString(AdminInbox.TABLE_NAME, columnNameList, queryTermList);	//Here's where the column names are fille din
 		
 		List<Object> objectList = new ArrayList<Object>();
-		objectList.add(newStatus);
-		
-		for(QueryTerm queryTerm : queryTermList)
-		{
-			objectList.add(queryTerm.getValue());
+		for(Object object : newValueList) {
+			objectList.add(object);	//First fill in new values
+		}
+	
+		for(QueryTerm queryTerm : queryTermList){
+			objectList.add(queryTerm.getValue());	//Second batch is conditions
 		}
 		
-	    Object[] parameters = objectList.toArray();
-		 
-		String queryTemplate = QueryStringBuilder.generateUpdateString(AdminInbox.TABLE_NAME, AdminInbox.getColumnName(AdminInbox.Columns.STATUS), queryTermList);
-	    
-	    int rowsAffected = jdbcTemplate.update(queryTemplate, parameters);
-	    
-		return rowsAffected;
-
+		Object[] parameters = objectList.toArray();
+		
+	
+		return jdbcTemplate.update(queryTemplate, parameters);//, objectTypeList.toArray());
 	}
 	
 	@Override
