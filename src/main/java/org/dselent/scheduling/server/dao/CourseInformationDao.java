51bd3412aa3f9daf2ed799f78ca13ad96d09e43a
Javier Marcos <xbc30ep450@gmail.com>
2018-02-02 20:01:15 -0500

    Added update 2.0 to courseInformationDao

--- src/main/java/org/dselent/scheduling/server/dao/CourseInformationDao.java
+++ src/main/java/org/dselent/scheduling/server/dao/CourseInformationDao.java
@@ -1,6 +1,11 @@
 package org.dselent.scheduling.server.dao;
+import java.util.List;
+
 import org.dselent.scheduling.server.model.CourseInformation;
+import org.dselent.scheduling.server.sqlutils.QueryTerm;
 
 public interface CourseInformationDao extends Dao<CourseInformation>{
-	//Add functions as needed
+	
+	//Updates a course, supports multiple fields
+	public int updateCourse(List<String> columnName, List<Object> newValue, List<QueryTerm> queryTermList);
 }
