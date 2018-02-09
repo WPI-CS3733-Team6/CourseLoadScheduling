111b543af4afc620029d27e3475632211c31dc62
drd387 <dannyd40@gmail.com>
2018-02-09 17:03:40 -0500

    fixed sectionId in instructorCourseLinkCart

--- src/main/java/org/dselent/scheduling/server/model/InstructorCourseLinkCart.java
+++ src/main/java/org/dselent/scheduling/server/model/InstructorCourseLinkCart.java
@@ -73,55 +73,68 @@ public class InstructorCourseLinkCart extends Model {
 		
 		return columnNameList;
 	}
-	
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public Integer getInstructorId() {
 		return instructorId;
 	}
+
 	public void setInstructorId(Integer instructorId) {
 		this.instructorId = instructorId;
 	}
-	public Integer getSectionId() {
+
+	public Integer getInstanceId() {
 		return instanceId;
 	}
-	public void setSectionId(Integer sectionId) {
-		this.instanceId = sectionId;
+
+	public void setInstanceId(Integer instanceId) {
+		this.instanceId = instanceId;
 	}
+
 	public Integer getStatus() {
 		return status;
 	}
+
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
+
 	public Timestamp getCreatedAt() {
 		return createdAt;
 	}
+
 	public void setCreatedAt(Timestamp createdAt) {
 		this.createdAt = createdAt;
 	}
+
 	public Timestamp getUpdatedAt() {
 		return updatedAt;
 	}
+
 	public void setUpdatedAt(Timestamp updatedAt) {
 		this.updatedAt = updatedAt;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((instructorId == null) ? 0 : instructorId.hashCode());
 		result = prime * result + ((instanceId == null) ? 0 : instanceId.hashCode());
+		result = prime * result + ((instructorId == null) ? 0 : instructorId.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
 		result = prime * result + ((updatedAt == null) ? 0 : updatedAt.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -141,15 +154,15 @@ public class InstructorCourseLinkCart extends Model {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (instructorId == null) {
-			if (other.instructorId != null)
-				return false;
-		} else if (!instructorId.equals(other.instructorId))
-			return false;
 		if (instanceId == null) {
 			if (other.instanceId != null)
 				return false;
 		} else if (!instanceId.equals(other.instanceId))
+			return false;
+		if (instructorId == null) {
+			if (other.instructorId != null)
+				return false;
+		} else if (!instructorId.equals(other.instructorId))
 			return false;
 		if (status == null) {
 			if (other.status != null)
@@ -163,10 +176,13 @@ public class InstructorCourseLinkCart extends Model {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "InstructorCourseLinkCart [id=" + id + ", instructorId=" + instructorId + ", sectionId=" + instanceId
+		return "InstructorCourseLinkCart [id=" + id + ", instructorId=" + instructorId + ", instanceId=" + instanceId
 				+ ", status=" + status + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
 	}
+	
+	
 	
 }
