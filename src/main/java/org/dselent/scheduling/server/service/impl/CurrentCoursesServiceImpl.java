de1a4366f07ffe62e87eec4545cd4cf4dc8dd3a9
drd387 <dannyd40@gmail.com>
2018-02-09 17:12:50 -0500

    uploading alex's work on CurrentCourseServiceImpl

--- src/main/java/org/dselent/scheduling/server/service/impl/CurrentCoursesServiceImpl.java
+++ src/main/java/org/dselent/scheduling/server/service/impl/CurrentCoursesServiceImpl.java
@@ -7,18 +7,23 @@ import java.util.List;
 import org.dselent.scheduling.server.dao.CourseInformationDao;
 import org.dselent.scheduling.server.dao.CourseInstanceDao;
 import org.dselent.scheduling.server.dao.CourseSectionDao;
+import org.dselent.scheduling.server.dao.InstructorsDao;
 import org.dselent.scheduling.server.dto.CourseDto;
 import org.dselent.scheduling.server.miscellaneous.Pair;
 import org.dselent.scheduling.server.model.CourseInformation;
+import org.dselent.scheduling.server.model.Instructor;
 import org.dselent.scheduling.server.model.InstructorCourseLinkRegistered;
 import org.dselent.scheduling.server.model.User;
 import org.dselent.scheduling.server.sqlutils.ColumnOrder;
 import org.dselent.scheduling.server.sqlutils.ComparisonOperator;
+import org.dselent.scheduling.server.sqlutils.LogicalOperator;
 import org.dselent.scheduling.server.sqlutils.QueryTerm;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class CurrentCoursesServiceImpl {
 	
+	@Autowired
+	private InstructorsDao instructorDao;
 	@Autowired
 	private CourseInformationDao masterCourseDao;
 	@Autowired
@@ -30,6 +35,7 @@ public class CurrentCoursesServiceImpl {
 		
 	}
 	
+	
 	//-----Did not finish what is below. We need to finish the courseDto first. Plus it's too late right now.-----//
 	//
 	//
@@ -39,7 +45,7 @@ public class CurrentCoursesServiceImpl {
 	//	-figure out the column name list. Do I need one for every single column in all three tables?
 	//		*probably not, but i need to figure out how to use the dto that was made in courseServiceImpl in this method
 	//		 like how do I make an instance of the actual dto. Do i need to make a builder here again? or just call that course() method?
-	/*
+	
 	public ArrayList<CourseDto> courses(Integer user_id) throws SQLException, Exception {
 		ArrayList<String> columnNameList = new ArrayList<String>();
 		columnNameList.add(CourseInformation.getColumnName(CourseInformation.Columns.COURSE_DESCRIPTION));
@@ -51,10 +57,10 @@ public class CurrentCoursesServiceImpl {
 		
 		ArrayList<QueryTerm> queryTermList = new ArrayList<QueryTerm>();
 
-		//Need to turn the user_id into an instructor_id... Below is just a place-holder
-		Integer instructor_id;
+		Integer instructorId = findInstructor(user_id);
+
 		QueryTerm idQueryTerm = new QueryTerm();
-		idQueryTerm.setValue(instructor_id);
+		idQueryTerm.setValue(instructorId);
 		idQueryTerm.setColumnName(InstructorCourseLinkRegistered.getColumnName(InstructorCourseLinkRegistered.Columns.INSTRUCTOR_ID));
 		idQueryTerm.setComparisonOperator(ComparisonOperator.EQUAL);
 		queryTermList.add(idQueryTerm);
@@ -64,8 +70,40 @@ public class CurrentCoursesServiceImpl {
 		List<CourseInformation> results = masterCourseDao.select(columnNameList, queryTermList, orderByList);
 		
 		ArrayList<CourseDto> courseDtoList = new ArrayList<CourseDto>();
-
+		
+		return courseDtoList;
 	}
-	return courseDtoList;
-	*/
+
+	
+		
+	public Integer findInstructor(Integer user_id) throws Exception {
+		Integer instructorId = 0;
+		
+		List<String> columnNameList = new ArrayList<String>();
+		columnNameList.add(Instructor.getColumnName(Instructor.Columns.USER_ID));
+		
+		List<QueryTerm> queryTermList = new ArrayList<>();
+		QueryTerm findInstructorQuery = new QueryTerm();
+		findInstructorQuery.setValue(user_id);
+		findInstructorQuery.setColumnName(Instructor.getColumnName(Instructor.Columns.USER_ID));
+		findInstructorQuery.setComparisonOperator(ComparisonOperator.EQUAL);
+		queryTermList.add(findInstructorQuery);
+		
+		List<Pair<String, ColumnOrder>> orderByList = new ArrayList<>();
+		
+		List<Instructor> results = instructorDao.select(columnNameList, queryTermList, orderByList);
+		
+		if (results.size() != 1)
+			throw new Exception("Testing");
+		
+		instructorId = results.get(0).getId();
+		
+		return instructorId;
+	}
+
+
+
+
+
+
 }
