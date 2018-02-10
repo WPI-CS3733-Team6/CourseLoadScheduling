c868bdd1d7a6334e2a6355eb544425355cff56bd
Josue Contreras <josudaco@hotmail.com>
2018-02-09 21:13:58 -0500

    Revert "Minor Adjustments"
    This reverts commit 2cdae38d87bf2d1cb221cddae519454effaef4ad.

--- src/main/java/org/dselent/scheduling/server/controller/impl/CourseControllerImpl.java
+++ src/main/java/org/dselent/scheduling/server/controller/impl/CourseControllerImpl.java
@@ -8,10 +8,14 @@ import org.dselent.scheduling.server.controller.CourseController;
 import org.dselent.scheduling.server.dto.CourseDto;
 import org.dselent.scheduling.server.miscellaneous.JsonResponseCreator;
 import org.dselent.scheduling.server.requests.CourseDetails;
+import org.dselent.scheduling.server.requests.Courses;
+import org.dselent.scheduling.server.requests.Login;
 import org.dselent.scheduling.server.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public class CourseControllerImpl implements CourseController{
 	
